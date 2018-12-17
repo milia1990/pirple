26f26ae42cf225228ac90c57354a67a031aff827
Sergey Milkin <milia19901990@gmail.com>
2018-12-17 21:23:56 +0300

    Pirple.My favorite song programm

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 16 22:52:20 2018
+
+My favorite song programm in Python3.7.1
+@author: sergeymilkin
+"""
+Title = "\"I Got Love (feat. Рем Дигга)\""
+Artist = "Miyagi & Эндшпиль"
+Year = 2016
+Album = "I Got Love (feat. Рем Дигга) - Single"
+Rating = "0+"
+Playlist = "Russia music"
+Duration_in_seconds = 276
+Duration_in_minutes = 4.36
+Genre = "Hip-Hop/Rap"
+
+print("This is my favorite song. Name :" , Title)
+print("Artist:",Artist)
+print("Year:",Year)
+print("Album:", Album)
+print("Rating:", Rating)
+print("Playlist:", Playlist)
+print("Duration in seconds:",Duration_in_seconds)
+print("Duration in minutes:", Duration_in_minutes)
+print("Genre:", Genre)
